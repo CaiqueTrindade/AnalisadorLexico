0d5da7b29c80dba16f65b0cb62623b66d1476c0e
CaiqueTrindade <usercah@gmail.com>
2019-11-13 00:48:43 -0300

    Retificação da main na classe Principal conforme o atual andamento do projeto

--- src/Principal.java
+++ src/Principal.java
@@ -1,104 +1,62 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Principal {
     public static void main(String[] args) {
 
-        try {
-            InputStream entrada = new FileInputStream("input\\entrada1.txt");
+        File input_diretorio = new File("\\input");
 
-            int c, estado_atual, linha_atual;
-            String lista_tokens = "", caractere_atual;
+        // Verifica se a pasta "input" existe no diretório do projeto
+        if (input_diretorio.exists()) {
 
-            estado_atual = 0;
-            linha_atual = 0;
+            // Verifica se a pasta "input" possui arquivos
+            if (input_diretorio.listFiles().length > 0) {
 
-            // Lê o próximo caractere em ASCII
-            c = entrada.read();
+                List<File> entradas_validas = new ArrayList();
 
-            boolean repete = true;
+                // Varre a lista de arquivos da pasta "input" em busca de arquivos de entrada válidos, isto é, que estejam dentro do padrão de entrada
+                for (File input: input_diretorio.listFiles())
+                    if (input.isFile() && input.getName().matches("^(input)\\d+(.txt)$"))
+                        entradas_validas.add(input);
 
+                // Se houver arquivos válidos
+                if (entradas_validas.size() > 0) {
 
-            while (repete) {
+                    System.out.println("Arquivos válidos de entrada encontrados: " + entradas_validas.size());
 
-                // Verdadeiro quando o caractere atual é diferente de um caractere de retorno ao início da linha \r
-                if (c != 13) {
-
-                    // Transforma o ASCII em seu respectivo caractere e o armazena em uma String
-                    caractere_atual = Character.toString((char) c);
-
-                    // Simulação do autômato
-                    switch (estado_atual) {
-                        case 0:
-                            if (caractere_atual.matches("^([a-z]|[A-Z])$")) {
-                                estado_atual = 1;
-                                lista_tokens += "" + linha_atual + " " + caractere_atual;
-                            }
-                            else if (c == 10) {
-                                linha_atual ++;
-                            }
-                            // Caso de erro (quando o caractere não faz parte da gramática)
-                            else if (c != 9 && c != 32 && c != -1) {
-                                estado_atual = -1;
-                                lista_tokens += "" + linha_atual + " " + caractere_atual;
-                            }
-
-                            if (c != -1)
-                                c = entrada.read();
-                            else
-                                repete = false;
-                            break;
-                        case 1:
-                            if (caractere_atual.matches("^([a-z]|[A-Z]|[0-9]|_)$")) {
-                                lista_tokens += caractere_atual;
-                                c = entrada.read();
-                            }
-                            // Verdadeiro quando o caractere atual é algum delimitador (incompleto e provavelmente sofrerá mudanças significativas)
-                            else  if (c == 9 || c == 32 || c == 10 || c == -1) {
-                                String tabela_aux[] = lista_tokens.split("\n");
-                                String ultima_linha = tabela_aux[tabela_aux.length - 1];
-
-                                if (ultima_linha.split(" ")[1].matches("^(var|const|typedef|struct|extends|procedure|function|start|return|if|else|then|while|read|print|int|real|boolean|string|true|false|global|local)$"))
-                                    lista_tokens += " " + ultima_linha.split(" ")[1] + " (palavra reservada)\n";
-                                else
-                                    lista_tokens += " identificador\n";
-
-                                estado_atual = 0;
-
-                                if (c == 9 || c == 32)
-                                    c = entrada.read();
-                            }
-                            // Caso de erro
-                            else {
-                                lista_tokens += caractere_atual;
-                                estado_atual = -1;
-                                c = entrada.read();
-                            }
-                            break;
-                        case -1:
-                            if (c == 9 || c == 32 || c == 10 || c == -1){
-                                lista_tokens += " ???\n";
-                                estado_atual = 0;
-
-                                if (c == 9 || c == 32)
-                                    c = entrada.read();
-                            }
-                            else {
-                                lista_tokens += caractere_atual;
-                                c = entrada.read();
-                            }
+                    // Percorre a lista das entradas válidas
+                    Iterator it = entradas_validas.iterator();
+                    while (it.hasNext()) {
+                        File input = (File) it.next();
+                        System.out.println("Lendo e analisando lexicalmente o arquivo: " + input.getName());
+                        AnalisadorLexico al = null;
+                        try {
+                            // Passa o arquivo de entrada para a classe que fará a leitura e a análise léxica do código-fonte
+                            al = new AnalisadorLexico(input);
+                            // Solicita que o AnalisadorLexico faça a análise
+                            al.executarAnalise();
+                            // Solicita que o AnalisadorLexico faça a escrita do arquivo de saída com os tokens e os erros encontrados
+                            al.escreverEmArquivo();
+                        } catch (FileNotFoundException e) {
+                            System.err.println("Arquivo de entrada não encontrado.");
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            System.err.println("Falha na leitura do arquivo de entrada ou na escrita do arquivo de saída.");
+                            e.printStackTrace();
+                        }
                     }
                 }
-                else {
-                    c = entrada.read();
-                }
+                else
+                    System.out.print("Não foram encontrados arquivos válidos na pasta de entrada \"input\".");
             }
-
-            System.out.print(lista_tokens);
-            entrada.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            else
+                System.out.print("A pasta de entrada \"input\" está vazia.");
         }
+        else
+            System.err.print("A pasta de entrada \"input\" não foi localizada no diretório do projeto.");
     }
 }
