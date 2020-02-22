62e6115cd6d6dadac84d8d15745ed8abadea13a0
felipedamasceno <felipedamasceno97@gmail.com>
2020-02-22 11:56:07 -0300

    add metade dos conjuntos manualmente

--- src/ConstrutorConjunto.java
+++ src/ConstrutorConjunto.java
@@ -9,19 +9,37 @@ public class ConstrutorConjunto {
     private Map<String,LinkedHashSet<String>> conjuntoFinal = new HashMap<String, LinkedHashSet<String>>();
 
     public ConstrutorConjunto(){
-        conjuntos.put("PStruct", new ArrayList<String>(Arrays.asList("typedef", "E")));
-        conjuntos.put("SStruct", new ArrayList<String>(Arrays.asList("PVar","PGeraFuncaoeProcedure","PStart")));
-        conjuntos.put("PVar", new ArrayList<String>(Arrays.asList("one", "two")));
-        conjuntos.put("PGeraFuncaoeProcedure", new ArrayList<String>(Arrays.asList("three", "four")));
-        conjuntos.put("PStart", new ArrayList<String>(Arrays.asList("five", "PStruct")));
+        conjuntos.put("PProcedimento", new ArrayList<String>(Arrays.asList("procedure")));
+        conjuntos.put("PParametro", new ArrayList<String>(Arrays.asList("PTipo")));
+        conjuntos.put("PPara1", new ArrayList<String>(Arrays.asList(",", ")")));
+        conjuntos.put("PPara2", new ArrayList<String>(Arrays.asList("[", "E")));
+        conjuntos.put("PPara3", new ArrayList<String>(Arrays.asList("[", "E")));
+        conjuntos.put("PF2", new ArrayList<String>(Arrays.asList("{")));
+        conjuntos.put("PConst", new ArrayList<String>(Arrays.asList("const")));
+        conjuntos.put("PTipoConst", new ArrayList<String>(Arrays.asList("PTipo")));
+        conjuntos.put("PIndiceVetor", new ArrayList<String>(Arrays.asList("int pos", "PIdentificador")));
+        conjuntos.put("PEscopo", new ArrayList<String>(Arrays.asList("local", "global")));
+        conjuntos.put("PIdentificadorSemFuncao", new ArrayList<String>(Arrays.asList("PEscopo", "Identificador")));
+        conjuntos.put("PExpressaoAritmetica", new ArrayList<String>(Arrays.asList("PT", "PIdentificadorAritmetico", "++", "--")));
 
-        conjuntos.put("PAlgo", new ArrayList<String>(Arrays.asList("SVar","PGeraFuncaoeProcedure","SStart")));
-        conjuntos.put("SVar", new ArrayList<String>(Arrays.asList("one", "two")));
-        conjuntos.put("SStart", new ArrayList<String>(Arrays.asList("five", "SStruct")));
+
+        conjuntos.put("SProcedimento", new ArrayList<String>(Arrays.asList("SGeraFuncaoeProcedure","PGeraFuncaoeProcedure")));
+        conjuntos.put("SParametro", new ArrayList<String>(Arrays.asList("PTipo")));
+        conjuntos.put("SPara1", new ArrayList<String>(Arrays.asList("SParametro")));
+        conjuntos.put("SPara2", new ArrayList<String>(Arrays.asList("PPara1")));
+        conjuntos.put("SPara3", new ArrayList<String>(Arrays.asList("SPara2")));
+        conjuntos.put("SF2", new ArrayList<String>(Arrays.asList("SPara1")));
+        conjuntos.put("SConst", new ArrayList<String>(Arrays.asList("PStruct", "PVar", "PGeraFuncaoeProcedure", "PStart")));
+        conjuntos.put("STipoConst", new ArrayList<String>(Arrays.asList("SConst", "SConst3")));
+
     }
 
-    public void primeiro(String nTerminal){
-        nTerminal = "P".concat(nTerminal);
+    public Map<String, ArrayList<String>> getConjuntos(){
+        return conjuntos;
+    }
+
+
+    public void conjuntoBuilder(String nTerminal){
         System.out.println(nTerminal);
         ArrayList<String> conjunto = conjuntos.get(nTerminal);
         System.out.println(conjunto);
@@ -30,17 +48,6 @@ public class ConstrutorConjunto {
         System.out.println(conjuntoSemRepeticao);
         conjuntoFinal.put(nTerminal, conjuntoSemRepeticao);
 
-    }
-
-    public void seguinte(String nTerminal){
-        nTerminal = "S".concat(nTerminal);
-        System.out.println(nTerminal);
-        ArrayList<String> conjunto = conjuntos.get(nTerminal);
-        System.out.println(conjunto);
-        conjunto = nTerminalToTerminal(conjunto);
-        LinkedHashSet conjuntoSemRepeticao = new LinkedHashSet<>(conjunto);
-        System.out.println(conjuntoSemRepeticao);
-        conjuntoFinal.put(nTerminal, conjuntoSemRepeticao);
     }
 
     public ArrayList<String> nTerminalToTerminal(ArrayList<String> conjunto){
@@ -73,12 +80,15 @@ public class ConstrutorConjunto {
 
     public static void main(String[] args) {
         ConstrutorConjunto c = new ConstrutorConjunto();
-        c.seguinte("Struct");
-        c.primeiro("Algo");
+        for( Map.Entry<String, ArrayList<String>> entry : c.getConjuntos().entrySet()) {
+            String key = entry.getKey();
+
+            c.conjuntoBuilder(key);
+        }
         c.save("teste");
 
         Conjunto conj = new Conjunto("teste");
-        conj.seguinte("Struct");
-        conj.primeiro("Algo");
+        conj.seguinte("Parametro");
+        conj.primeiro("Procedimento");
     }
 }
