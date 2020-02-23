b35657aa118bc67b6bef29ace9ab20b7df37fb16
CaiqueTrindade <usercah@gmail.com>
2020-02-22 23:07:21 -0300

    Retificação da função sincronizar

--- src/AnalisadorSintatico.java
+++ src/AnalisadorSintatico.java
@@ -17,40 +17,50 @@ public class AnalisadorSintatico {
             token = tokens.get(0);
             tokens.remove(0);
         }
-        else
-            token = null;
+        else token = null;
     }
 
-    private void sincronizar(List<LinkedHashSet> tokens_sincronizacao) {
+    private void sincronizar(String conjuntos_primeiro, String conjuntos_seguinte, String lexemas) {
 
-        boolean encontrou = false;
-        boolean eof = (token == null);
+        ArrayList<String> tokens_sincronizacao = new ArrayList<>();
 
-        while (!encontrou && !eof) {
-            Iterator it = tokens_sincronizacao.iterator();
-            while(it.hasNext() && !encontrou) {
-                LinkedHashSet conjunto = (LinkedHashSet) it.next();
+        if (conjuntos_primeiro != null) {
+            String aux[] = conjuntos_primeiro.split("#");
+            for (int i = 0; i < aux.length; i++) tokens_sincronizacao.addAll(conjuntos_P_S.primeiro(aux[i]));
+        }
+
+        if (conjuntos_seguinte != null) {
+            String aux[] = conjuntos_seguinte.split("#");
+            for (int i = 0; i < aux.length; i++) tokens_sincronizacao.addAll(conjuntos_P_S.seguinte(aux[i]));
+        }
+
+        if(lexemas != null) {
+            String aux[] = conjuntos_seguinte.split("#");
+            for (int i = 0; i < aux.length; i++) if (!tokens_sincronizacao.contains(aux[i])) tokens_sincronizacao.add(aux[i]);
+        }
+
+        if (tokens_sincronizacao.size() > 0) {
+
+            boolean encontrou = false;
+            boolean eof = (token == null);
+
+            while (!encontrou && !eof) {
                 if (token.getTipo() == 3) {
-                    if (conjunto.contains("Id"))
+                    if (tokens_sincronizacao.contains("Id"))
                         encontrou = true;
-                }
-                else if (token.getTipo() == 2 && Integer.parseInt(token.getLexema()) >= 0) {
-                    if (conjunto.contains("IntPos"))
+                } else if (token.getTipo() == 2 && Integer.parseInt(token.getLexema()) >= 0) {
+                    if (tokens_sincronizacao.contains("IntPos"))
                         encontrou = true;
-                }
-                else if(token.getTipo() == 0 && token.getLexema().matches("^(true|false)$")) {
-                    if (conjunto.contains("Boolean"))
+                } else if (token.getTipo() == 0 && token.getLexema().matches("^(true|false)$")) {
+                    if (tokens_sincronizacao.contains("Boolean"))
                         encontrou = true;
-                }
-                else if(token.getTipo() == 11) {
-                    if (conjunto.contains("String"))
+                } else if (token.getTipo() == 11) {
+                    if (tokens_sincronizacao.contains("String"))
                         encontrou = true;
-                }
-                else if(token.getTipo() == 1 || token.getTipo() == 2) {
-                    if (conjunto.contains("Numero"))
+                } else if (token.getTipo() == 1 || token.getTipo() == 2) {
+                    if (tokens_sincronizacao.contains("Numero"))
                         encontrou = true;
-                }
-                else if (conjunto.contains(token.getLexema())) {
+                } else if (tokens_sincronizacao.contains(token.getLexema())) {
                     encontrou = true;
                 }
             }
@@ -59,7 +69,6 @@ public class AnalisadorSintatico {
                 if (token == null) eof = true;
             }
         }
-
     }
 
     public void executarAnalise() {
