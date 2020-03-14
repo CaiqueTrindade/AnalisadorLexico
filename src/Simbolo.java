b3a523db27c3834fdc750a9c4d844dd0f71f90ad
CaiqueTrindade <usercah@gmail.com>
2020-03-14 13:09:11 -0300

    Retificações na classe Simbolo

--- src/Simbolo.java
+++ src/Simbolo.java
@@ -13,6 +13,7 @@ public class Simbolo {
     private String identificador;                   // Identificador da função/procedimento/estrutura
     private int tipo;                               // Tipo (START = 0, FUNCTION = 1, PROCEDURE = 2, STRUCT = 3)
     private String parametros;                      // Lista de parâmetros (Ex: "int:a,real:b,string:c")
+    private String tipo_retorno;                    // Tipo do retorno da função, void se for um procedimento
     private Map<String, String> var_local;          // Hash Map com as variáveis ou campos pertencentes a função/procedimento/estrutura
 
     // Constantes referentes ao tipo do “símbolo”
@@ -26,7 +27,34 @@ public class Simbolo {
     public Simbolo(String identificador, int tipo) {
         this.identificador = identificador;
         this.tipo = tipo;
+        this.tipo_retorno = (tipo == PROCEDURE)?"void":null;
         this.var_local = new HashMap<String, String>();
+    }
+
+    public Simbolo(String identificador, int tipo, String tipo_retorno) {
+        this.identificador = identificador;
+        this.tipo = tipo;
+        this.tipo_retorno = tipo_retorno;
+        this.var_local = new HashMap<String, String>();
+    }
+
+    /**
+     * Atribui um tipo de retorno para símbolos que sejam funções ou procedimentos. Nesse último caso o retorno deve ser
+     * void.
+     *
+     * @param tipo_retorno tipo do retorno (primitivo ou definido por um typedef), void para procedimentos.
+     */
+    public void setTipo_retorno(String tipo_retorno) {
+        this.tipo_retorno = tipo_retorno;
+    }
+
+    /**
+     * Retorna o tipo do retorno da função (void para procedimentos).
+     *
+     * @return tipo do retorno (void para procedimentos)
+     */
+    public String getTipo_retorno() {
+        return tipo_retorno;
     }
 
     /**
@@ -48,10 +76,14 @@ public class Simbolo {
     }
 
     /**
-     * Método para adicionar uma variável ou campo pertencente a função/procedimento/estrutura
+     * Método para adicionar uma variável ou campo pertencente a função/procedimento/estrutura.
+     *
+     * Exemplo de chamada para inserção de uma variável do tipo int: addVariavel("nome_da_variavel","tipo:int;categoria:variavel")
+     * Exemplo de chamada para inserção de uma variável que é um vetor de inteiros: addVariavel("nome_da_variavel","tipo:int;categoria:vetor")
+     * Exemplo de chamada para inserção de uma variável que é uma matriz de inteiros: addVariavel("nome_da_variavel","tipo:int;categoria:matriz")
      *
      * @param id_variavel identificador (nome) da variável ou campo
-     * @param atributos_variavel atributos pertinentes à variável (tipo, categoria, entre outros)
+     * @param atributos_variavel atributos pertinentes à variável (tipo, categoria, entre outros), devem seguir o exemplo descrito na documentação
      * @return true se inserido com sucesso, false se já houver alguma variável/campo com mesmo nome (identificador)
      */
     public boolean addVariavel(String id_variavel, String atributos_variavel) {
@@ -82,7 +114,7 @@ public class Simbolo {
     public boolean addParametro(String tipo_parametro, String id_parametro) {
         if (!var_local.containsKey(id_parametro)) {
             var_local.put(id_parametro, "tipo:" + tipo_parametro);
-            parametros += ";" + tipo_parametro + ":" + id_parametro;
+            parametros = (parametros == null) ? "" + tipo_parametro + ":" + id_parametro : parametros + ";" + tipo_parametro + ":" + id_parametro;
             return true;
         }
         return false;
